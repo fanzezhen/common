1a18c437734c459746cc55759c3f94fc36f4e710
fanzezhen <zzfan@sstir.cn>
2020-07-30 16:24:39 +0800

    异常接口；mp-config；验证码接口；oauth接口

---
+++ common-mp/src/main/java/com/github/fanzezhen/common/mp/config/MyMetaObjectHandler.java
@@ -0,0 +1,25 @@
+package com.github.fanzezhen.common.mp.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.debug("start insert fill ....");
+        this.fillStrategy(metaObject, "createTime", LocalDateTime.now()); // 也可以使用(3.3.0 该方法有bug请升级到之后的版本如`3.3.1.8-SNAPSHOT`)
+        this.fillStrategy(metaObject, "updateTime", LocalDateTime.now()); // 也可以使用(3.3.0 该方法有bug请升级到之后的版本如`3.3.1.8-SNAPSHOT`)
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.debug("start update fill ....");
+        this.fillStrategy(metaObject, "updateTime", LocalDateTime.now()); // 也可以使用(3.3.0 该方法有bug请升级到之后的版本如`3.3.1.8-SNAPSHOT`)
+    }
+}
