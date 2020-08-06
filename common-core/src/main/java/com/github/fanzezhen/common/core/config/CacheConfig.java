8fbbe2f23e58213890727d67f779847a60433864
fanzezhen <zzfan@sstir.cn>
2020-08-06 16:06:44 +0800

    redis缓存配置

--- common-core/src/main/java/com/github/fanzezhen/common/core/config/CacheConfig.java
+++ common-core/src/main/java/com/github/fanzezhen/common/core/config/CacheConfig.java
@@ -1,13 +1,34 @@
 package com.github.fanzezhen.common.core.config;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+import javax.annotation.Resource;
+import java.time.Duration;
 
 /**
  * 启用缓存
+ *
  * @author fanzezhen
  **/
 @Configuration
 @EnableCaching
+//@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600 * 12)// Redis最大过期时间
 public class CacheConfig {
+    @Resource
+    private RedisConnectionFactory redisConnectionFactory;
+
+    @Bean
+    public CacheManager cacheManager() {
+        return RedisCacheManager
+                .builder(redisConnectionFactory)
+                .cacheDefaults(RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(15)))
+                .transactionAware()
+                .build();
+    }
 }
