1a5a467d8428639c0caa26f1f4244342a929f9e0
fanzezhen <zzfan@sstir.cn>
2020-08-06 15:05:57 +0800

    security authorities为空时处理

--- common-security/src/main/java/com/github/fanzezhen/common/security/model/SysUserDetail.java
+++ common-security/src/main/java/com/github/fanzezhen/common/security/model/SysUserDetail.java
@@ -7,16 +7,10 @@ import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.CredentialsContainer;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.util.Assert;
 
-import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.*;
-import java.util.function.Function;
 
 @Slf4j
 @Getter
@@ -105,10 +99,10 @@ public class SysUserDetail extends User implements CredentialsContainer {
     private String appCode;
 
     private Set<GrantedAuthority> authorities;
-    private boolean accountNonExpired;
-    private boolean accountNonLocked;
-    private boolean credentialsNonExpired;
-    private boolean enabled;
+    private boolean accountNonLocked = true;    // 默认返回的是false，翻译成人话就是：是否不上锁，否，即上锁。
+    private boolean enabled = true; // 用户是否被有效，false时为禁用状态
+    private boolean accountNonExpired = true;  // User account has expired
+    private boolean credentialsNonExpired = true;  // User credentials have expired
 
     private Collection<String> roleNames;
     private Collection<String> roleIds;
@@ -118,21 +112,12 @@ public class SysUserDetail extends User implements CredentialsContainer {
 
     public SysUserDetail(SysUserDto sysUserDto, Collection<? extends GrantedAuthority> authorities) {
         super(sysUserDto.getUsername(), sysUserDto.getPassword(), authorities);
+        if (this.authorities == null) {
+            log.warn("user：{} authorities 为 null", sysUserDto.getUsername());
+            this.authorities = new HashSet<>();
+        }
         BeanConverterUtil.copy(sysUserDto, this);
     }
-
-    private static SortedSet<GrantedAuthority> sortAuthorities(Collection<? extends GrantedAuthority> authorities) {
-        Assert.notNull(authorities, "Cannot pass a null GrantedAuthority collection");
-        SortedSet<GrantedAuthority> sortedAuthorities = new TreeSet<>(new AuthorityComparator());
-
-        for (GrantedAuthority grantedAuthority : authorities) {
-            Assert.notNull(grantedAuthority, "GrantedAuthority list cannot contain any null elements");
-            sortedAuthorities.add(grantedAuthority);
-        }
-
-        return sortedAuthorities;
-    }
-
 
     public String toString() {
         StringBuilder sb = new StringBuilder();
@@ -161,111 +146,4 @@ public class SysUserDetail extends User implements CredentialsContainer {
 
         return sb.toString();
     }
-
-    @Override
-    public void eraseCredentials() {
-        setPassword(null);
-    }
-
-    public static class UserBuilder {
-        private String username;
-        private String password;
-        private List<GrantedAuthority> authorities;
-        private boolean accountExpired;
-        private boolean accountLocked;
-        private boolean credentialsExpired;
-        private boolean disabled;
-        private Function<String, String> passwordEncoder;
-
-        private UserBuilder() {
-            this.passwordEncoder = (password) -> {
-                return password;
-            };
-        }
-
-        public UserBuilder username(String username) {
-            Assert.notNull(username, "username cannot be null");
-            this.username = username;
-            return this;
-        }
-
-        public UserBuilder password(String password) {
-            Assert.notNull(password, "password cannot be null");
-            this.password = password;
-            return this;
-        }
-
-        public UserBuilder passwordEncoder(Function<String, String> encoder) {
-            Assert.notNull(encoder, "encoder cannot be null");
-            this.passwordEncoder = encoder;
-            return this;
-        }
-
-        public UserBuilder roles(String... roles) {
-            List<GrantedAuthority> authorities = new ArrayList<>(roles.length);
-            int var4 = roles.length;
-
-            for (String role : roles) {
-                Assert.isTrue(!role.startsWith("ROLE_"), () -> role + " cannot start with ROLE_ (it is automatically added)");
-                authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
-            }
-
-            return this.authorities(authorities);
-        }
-
-        public UserBuilder authorities(GrantedAuthority... authorities) {
-            return this.authorities(Arrays.asList(authorities));
-        }
-
-        public UserBuilder authorities(Collection<? extends GrantedAuthority> authorities) {
-            this.authorities = new ArrayList<>(authorities);
-            return this;
-        }
-
-        public UserBuilder authorities(String... authorities) {
-            return this.authorities(AuthorityUtils.createAuthorityList(authorities));
-        }
-
-        public UserBuilder accountExpired(boolean accountExpired) {
-            this.accountExpired = accountExpired;
-            return this;
-        }
-
-        public UserBuilder accountLocked(boolean accountLocked) {
-            this.accountLocked = accountLocked;
-            return this;
-        }
-
-        public UserBuilder credentialsExpired(boolean credentialsExpired) {
-            this.credentialsExpired = credentialsExpired;
-            return this;
-        }
-
-        public UserBuilder disabled(boolean disabled) {
-            this.disabled = disabled;
-            return this;
-        }
-
-        public UserDetails build() {
-            String encodedPassword = this.passwordEncoder.apply(this.password);
-            return new User(this.username, encodedPassword, !this.disabled, !this.accountExpired,
-                    !this.credentialsExpired, !this.accountLocked, this.authorities);
-        }
-    }
-
-    private static class AuthorityComparator implements Comparator<GrantedAuthority>, Serializable {
-        private static final long serialVersionUID = 510L;
-
-        private AuthorityComparator() {
-        }
-
-        public int compare(GrantedAuthority g1, GrantedAuthority g2) {
-            if (g2.getAuthority() == null) {
-                return -1;
-            } else {
-                return g1.getAuthority() == null ? 1 : g1.getAuthority().compareTo(g2.getAuthority());
-            }
-        }
-    }
-
 }
