26ba566f14371498f675ca8d43d26a3bcfe8f147
fanzezhen <zzfan@sstir.cn>
2020-09-09 13:37:19 +0800

    优化common-core/src/main/java/com/github/fanzezhen/common/core/util/BeanConverterUtil.java

--- common-core/src/main/java/com/github/fanzezhen/common/core/util/BeanConverterUtil.java
+++ common-core/src/main/java/com/github/fanzezhen/common/core/util/BeanConverterUtil.java
@@ -2,10 +2,15 @@ package com.github.fanzezhen.common.core.util;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.bean.copier.CopyOptions;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cglib.beans.BeanMap;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 
 @Slf4j
@@ -137,4 +142,79 @@ public class BeanConverterUtil {
 
         return tRes;
     }
+
+    /**
+     * 将对象装换为map
+     *
+     * @param bean
+     * @return
+     */
+    public static <T> Map<String, Object> beanToMap(T bean) {
+        Map<String, Object> map = Maps.newHashMap();
+        if (bean != null) {
+            BeanMap beanMap = BeanMap.create(bean);
+            for (Object key : beanMap.keySet()) {
+                map.put(String.valueOf(key), beanMap.get(key));
+            }
+        }
+        return map;
+    }
+
+    /**
+     * 将map装换为javabean对象
+     *
+     * @param map
+     * @param bean
+     * @return
+     */
+    public static <T> T mapToBean(Map<String, Object> map, T bean) {
+        BeanMap beanMap = BeanMap.create(bean);
+        beanMap.putAll(map);
+        return bean;
+    }
+
+    /**
+     * 将List<T>转换为List<Map<String, Object>>
+     *
+     * @param objList
+     */
+    public static <T> List<Map<String, Object>> objectsToMaps(List<T> objList) {
+        List<Map<String, Object>> list = Lists.newArrayList();
+        if (objList != null && objList.size() > 0) {
+            Map<String, Object> map;
+            T bean;
+            for (T t : objList) {
+                bean = t;
+                map = beanToMap(bean);
+                list.add(map);
+            }
+        }
+        return list;
+    }
+
+    /**
+     * 将List<Map<String,Object>>转换为List<T>
+     *
+     * @param maps
+     * @param clazz
+     * @return
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     */
+    public static <T> List<T> mapsToObjects(List<Map<String, Object>> maps, Class<T> clazz)
+            throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        List<T> list = Lists.newArrayList();
+        if (maps != null && maps.size() > 0) {
+            Map<String, Object> map;
+            T bean;
+            for (Map<String, Object> stringObjectMap : maps) {
+                map = stringObjectMap;
+                bean = clazz.getDeclaredConstructor().newInstance();
+                mapToBean(map, bean);
+                list.add(bean);
+            }
+        }
+        return list;
+    }
+
 }
