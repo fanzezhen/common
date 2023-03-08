e2203c2f91a10e948bfa4b728fbfc05ad553e17d
fanzezhen <zezhen.fan@mobilemd.cn>
2023-03-08 16:46:31 +0800

    v2.1.2.1 1. 替换文本文件内容工具

--- common-core/src/main/java/com/github/fanzezhen/common/core/util/CommonUtil.java
+++ common-core/src/main/java/com/github/fanzezhen/common/core/util/CommonUtil.java
@@ -1,6 +1,9 @@
 package com.github.fanzezhen.common.core.util;
 
 import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.ArrayUtil;
+import cn.hutool.core.util.StrUtil;
 import cn.stylefeng.roses.kernel.model.exception.ServiceException;
 import cn.stylefeng.roses.kernel.model.exception.enums.CoreExceptionEnum;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +14,8 @@ import java.io.*;
 import java.lang.reflect.InvocationTargetException;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author zezhen.fan
@@ -44,13 +49,9 @@ public class CommonUtil {
         response.reset();
         // 设置强制下载不打开
         response.setContentType("application/force-download");
-        try {
-            // 设置文件名
-            response.addHeader("Content-Disposition", "attachment;fileName=" +
-                    new String(file.getName().getBytes(StandardCharsets.UTF_8), "8859_1"));
-        } catch (UnsupportedEncodingException e) {
-            throw new ServiceException(HttpStatus.INTERNAL_SERVER_ERROR.value(), "导出文件名设置失败！");
-        }
+        // 设置文件名
+        response.addHeader("Content-Disposition", "attachment;fileName=" +
+                new String(file.getName().getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1));
         byte[] buffer = new byte[1024];
         FileInputStream fis = null;
         BufferedInputStream bis = null;
@@ -83,5 +84,36 @@ public class CommonUtil {
                 }
             }
         }
+    }
+    public static void replaceInFile(String originPath, String targetPath, CharSequence target, CharSequence replacement) {
+        File file = new File(originPath);
+        if (!file.exists()){
+            return;
+        }
+        if (file.isFile()){
+            replaceInFile(file, targetPath, target, replacement);
+        }
+        File[] files = file.listFiles();
+        if (ArrayUtil.isEmpty(files)){
+            return;
+        }
+        if (!targetPath.endsWith(File.separator) && !targetPath.endsWith(StrUtil.BACKSLASH)) {
+            targetPath += File.separator;
+        }
+        for (File listFile : files) {
+            replaceInFile(listFile, targetPath + listFile.getName(), target, replacement);
+        }
+    }
+
+    private static void replaceInFile(File file, String targetFilePath, CharSequence target, CharSequence replacement) {
+        if (!file.isFile()){
+            return;
+        }
+        List<String> list = FileUtil.readUtf8Lines(file);
+        FileUtil.writeUtf8Lines(list.stream().map(s -> s.replace(target, replacement)).collect(Collectors.toList()), new File(targetFilePath));
+    }
+
+    public static void main(String[] args) {
+        replaceInFile("D:\\tmp\\2023年2月10日", "D:\\tmp\\2月10日", "INSERT INTO site_finance.", "INSERT INTO site_pro_finance.");
     }
 }
